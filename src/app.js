18c16d0089759e953a1631a9537e01c810691ed0
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2024-12-16 04:57:05 +0000

    Trabajo final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const button = document.querySelector("#Execute");
+
+button.addEventListener("click", domain);
+
+//write your code here
+
+function domain() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net"];
+  for (let first of pronoun) {
+    for (let second of adj) {
+      for (let third of noun) {
+        for (let fourth of extension) {
+          console.log(first + second + third + fourth);
+        }
+      }
+    }
+  }
+}
